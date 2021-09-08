4e40fbcf037b14bded696dda36215c340e31188f
Matt Nev <mattnev@microsoft.com>
2021-09-08 10:59:33 -0500

    create intial files after demo

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Hangman
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var words = new List<string>()
+            {
+               "Different",
+               "Goodbye",
+               "Greetings",
+
+            };
+
+            var wordbank = new WordBank();
+
+            var word = wordbank.GetWord();
+        }
+    }
+}
---
+++ WordBank.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hangman
+{
+    public class WordBank
+    {
+        private List<string> words;
+        public WordBank()
+        {
+            words = new List<string>()
+            {
+               "Hello",
+               "Goodbye",
+               "Greetings",
+
+            };
+
+            // same as above, different syntax
+                // words = new List<string>();
+                // words.Add("Hello");
+                // words.Add("Goodbye");
+
+        }
+        public WordBank(List<string> words)
+        {
+            this.words = words;
+        }
+
+        public string GetWord()
+        {
+            var random = new Random();
+            var index = random.Next(0, words.Count - 1);
+
+            return words[index];
+        }
+    }
+}
